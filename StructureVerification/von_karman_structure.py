b2be09121afce4cd624a91041e4566cea99bb7ee
Andreas Strøm Slyngstad <Andreas@Andreass-MacBook-Pro.local>
2016-10-07 14:36:15 +0200

    Eulerian implementations.. diverge for t = 2

--- StructureVerification/von_karman_structure.py
+++ StructureVerification/von_karman_structure.py
@@ -99,7 +99,7 @@ elif implementation =="2":
     G =rho_s*((1./k)*inner(w-w0,psi))*dx + rho_s*inner(dot(grad(0.5*(w+w0)),0.5*(w+w0)),psi)*dx + inner(s_s_n_l(0.5*(d+d0)),grad(psi))*dx \
     - inner(g,psi)*dx - dot(d-d0,phi)*dx + k*dot(0.5*(w+w0),phi)*dx
 
-#Full Eulerian description
+#Full Eulerian description  Full Implicit
 elif implementation == "3":
     bc1 = DirichletBC(VV.sub(0), ((0,0)),boundaries, 1)
     bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
@@ -111,38 +111,110 @@ elif implementation == "3":
     w0, d0 = split(w0d0)
     d_disp = Function(V)
 
+
+    #Fully implicit, gives significant damping to the system
     I = Identity(2)
     F_ = I - grad(d) #d here must be the same as in variational formula
     J_ = det(F_)
 
-    #WORKING BUT WITH DAMPING
     G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w), w), psi) \
     + inner(Venant_Kirchhof(d), grad(psi)) - J_*inner(g, psi) ) * dx \
     + inner((d - d0) + k*dot(grad(d), w) - k*w, phi) * dx
 
-    #Cranc Nic some
-    #G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
-    #+ inner(Venant_Kirchhof(0.5*(d + d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
-    #+ inner(d - d0 + k*dot(grad(d), w) - k*w0 , phi) * dx
+#Full Eulerian description Full Cran-Nic
+elif implementation == "4":
+        bc1 = DirichletBC(VV.sub(0), ((0,0)),boundaries, 1)
+        bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
+        bcs = [bc1,bc2]
+        psi, phi = TestFunctions(VV)
+        wd = Function(VV)
+        w, d = split(wd)
+        w0d0 = Function(VV)
+        w0, d0 = split(w0d0)
+        d_disp = Function(V)
 
-    #First order frac-step
-    # solving for n + 1/2
-    """
-    G_1 = ( J_*rho_s/(k/2)*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w0), w0), psi) \
-    + inner(Venant_Kirchhof(d0), grad(psi)) - J_*inner(g, psi) ) * dx \
-    + inner((d - d0) + k/2*dot(grad(d), w) - k/2*w, phi) * dx
+        #Full CrancNic Diverge after 2 period
+        I = Identity(2)
+        F_ = I - grad(0.5*(d+d0)) #d here must be the same as in variational formula
+        J_ = det(F_)
 
-    G = ( J_*rho_s/(k/2)*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w), w), psi) \
-    + inner(Venant_Kirchhof(d), grad(psi)) - J_*insner(g, psi) ) * dx \
-    + inner((d - d0) + k/2*dot(grad(d), w) - k/2*w, phi) * dx
+        ### TEST CODE
+        G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
+        + inner(0.5*(Venant_Kirchhof(d) + Venant_Kirchhof(d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
+        + (dot(d - d0, phi) + k*dot(dot(grad(d), w), phi ) \
+        - k*dot(0.5*(w+w0), phi) ) *dx
+        #+ inner(d - d0 + k*dot(grad(0.5*(d+d0)), 0.5*(w+w0)) - k*w) , phi) * dx
+
+        ### Middled d inside sigma arguent, q(0.5*(d+d0))  t > 2
+        #G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
+        #+ inner(Venant_Kirchhof(0.5*(d+d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
+        #+ (dot(d - d0, phi) + k*dot(dot(grad(0.5*(d+d0)), 0.5*(w+w0)), phi ) \
+        #- k*dot(w, phi) ) *dx
+        #+ inner(d - d0 + k*dot(grad(0.5*(d+d0)), 0.5*(w+w0)) - k*w) , phi) * dx
+
+        ### Middled d by 0.5*(q(d) + q(d0) ) t > 2
+        #G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
+        #+ inner(0.5*(Venant_Kirchhof(d) + Venant_Kirchhof(d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
+        #+ (dot(d - d0, phi) + k*dot(dot(grad(0.5*(d+d0)), 0.5*(w+w0)), phi ) \
+        #- k*dot(w, phi) ) *dx
+        #+ inner(d - d0 + k*dot(grad(0.5*(d+d0)), 0.5*(w+w0)) - k*w) , phi) * dx
+
+        # FULL CRAC DIVERGES,
+        #G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(0.5*(w+w0)), 0.5*(w + w0)), psi) \
+        #+ inner(0.5*(Venant_Kirchhof(d) + Venant_Kirchhof(d0)), grad(psi)) - J_*inner(g, psi) ) * dx \
+        #+ inner(d - d0 + k*dot(grad(0.5*(d+d0)), 0.5*(w+w0)) - k*0.5*(w+w0) , phi) * dx
+
+
+#Full Eulerian description Forward Euler
+elif implementation == "5":
+        bc1 = DirichletBC(VV.sub(0), ((0,0)),boundaries, 1)
+        bc2 = DirichletBC(VV.sub(1), ((0,0)),boundaries, 1)
+        bcs = [bc1,bc2]
+        psi, phi = TestFunctions(VV)
+        wd = Function(VV)
+        w, d = split(wd)
+        w0d0 = Function(VV)
+        w0, d0 = split(w0d0)
+        d_disp = Function(V)
+
+        #Full CrancNic Diverge after 2 period
+        I = Identity(2)
+        F_ = I - grad(d0) #d here must be the same as in variational formula
+        J_ = det(F_)
+
+
+        G = ( J_*rho_s/k*inner(w - w0, psi) + J_*rho_s*inner(dot(grad(w0), w0), psi) \
+        + inner(Venant_Kirchhof(d0), grad(psi)) - J_*inner(g, psi) ) * dx \
+        + inner(d - d0 + k*dot(grad(d0), w0) - k*w0 , phi) * dx
+
+elif implementation == "6":
+    bc1 = DirichletBC(V, ((0,0)),boundaries, 1)
+    bcs = [bc1]
+    psi = TestFunction(V)
+    d = Function(V)
+    d0 = Function(V)
+    d1 = Function(V)
+    d_disp = Function(V)
     """
+    G = rho_s*((1./k**2)*inner(d - 2*d0 + d1,psi))*dx \
+    + rho_s*inner(dot(grad(0.5*(d + d1) ), (d - d1)/(2.*k)), psi)*dx \
+    + inner(Venant_Kirchhof(0.5*(d + d1)), grad(psi))*dx - inner(g,psi)*dx
+    """
+    I = Identity(2)
+    F_ = I - grad(0.5*(d + d1)) #d here must be the same as in variational formula
+    J_ = det(F_)
+
+    G = J_*rho_s*((1./k**2)*inner(d - 2*d0 + d1, psi))*dx \
+    + J_*rho_s*inner(dot(grad(0.5*(d + d1) ), (d - d1)/(2.*k)) ,psi)*dx \
+    + inner(Venant_Kirchhof(0.5*(d + d1) ),grad(psi))*dx - J_*inner(g,psi)*dx
+
 
 dis_x = []
 dis_y = []
-
+time = []
 T = 5;
 t = 0
-time = np.linspace(0,T,(T/dt)+1)
+
 print "time len",len(time)
 ###### REMOVE BEFORE MERGE
 dis_1 = File("./displace_1/dis.pvd")
@@ -156,6 +228,7 @@ from time import sleep
 count = 0
 while t < T:
     print "Time: ",t
+    time.append(t)
     if implementation == "1":
         solve(G == 0, d, bcs, solver_parameters={"newton_solver": \
         {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
@@ -177,13 +250,12 @@ while t < T:
         dis_y.append(d(coord)[1])
 
     elif implementation == "3":
-        #solve(G_1 == 0, wd, bcs, solver_parameters={"newton_solver": \
-        #{"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
-        #w0d0.assign(wd)
         solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
         {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
         w0d0.assign(wd)
         w0,d0 = w0d0.split(True)
+        dis_x.append(d0(coord)[0])
+        dis_y.append(d0(coord)[1])
         d_disp.vector()[:] = w0.vector()[:]*float(k)
         dis_3 << d0
         ALE.move(mesh, d_disp)
@@ -195,11 +267,36 @@ while t < T:
         {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
         w0d0.assign(wd)
         w0,d0 = w0d0.split(True)
-        w,d = wd.split(True)
-        dis_4 << d0
+        dis_x.append(d0(coord)[0])
+        dis_y.append(d0(coord)[1])
         d_disp.vector()[:] = w0.vector()[:]*float(k)
+        dis_4 << d0
         ALE.move(mesh, d_disp)
         mesh.bounding_box_tree().build(mesh)
+
+    elif implementation == "5":
+        solve(G == 0, wd, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        w0d0.assign(wd)
+        w0,d0 = w0d0.split(True)
+        dis_x.append(d0(coord)[0])
+        dis_y.append(d0(coord)[1])
+        d_disp.vector()[:] = w0.vector()[:]*float(k)
+        dis_5 << d0
+        ALE.move(mesh, d_disp)
+        mesh.bounding_box_tree().build(mesh)
+
+    elif implementation == "6":
+        solve(G == 0, d, bcs, solver_parameters={"newton_solver": \
+        {"relative_tolerance": 1E-9,"absolute_tolerance":1E-9,"maximum_iterations":100,"relaxation_parameter":1.0}})
+        d1.assign(d0)
+        d0.assign(d)
+        dis_6 << d
+        d_disp.vector()[:] = d0.vector()[:] - d1.vector()[:]
+        ALE.move(mesh, d_disp)
+        mesh.bounding_box_tree().build(mesh)
+        dis_x.append(d(coord)[0])
+        dis_y.append(d(coord)[1])
 
 
     count += 1
@@ -212,12 +309,16 @@ if implementation == "1":
 elif implementation == "2":
     title = plt.title("Double space")
 elif implementation == "3":
-    title = plt.title("Single space solving w")
-    sys.exit(0)
+    title = plt.title("Eulerian")
 elif implementation == "4":
-    sys.exit(0)
+    title = plt.title("Eulerian cranc-adam/bash combined Oasis style")
 
-#plt.plot(time,dis_x,);title; plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
+
+plt.figure(1)
+plt.plot(time,dis_x,);title; plt.ylabel("Displacement x");plt.xlabel("Time");plt.grid();
+plt.savefig("Run_x.jpg")
 #plt.show()
-#plt.plot(time,dis_y);title;plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
+plt.figure(2)
+plt.plot(time,dis_y);title;plt.ylabel("Displacement y");plt.xlabel("Time");plt.grid();
+plt.savefig("Run_y.jpg")
 #plt.show()
